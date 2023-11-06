6de826e875b9887c7f29fcc17ad42655cd2e2c94
@JuanR5 <juan_p.rivera@uao.edu.co>
2023-11-05 23:45:43 -0500

    Banana dataset

--- smartg/src/smart_g/detectorA.py
+++ smartg/src/smart_g/detectorA.py
@@ -44,7 +44,7 @@ class DetectorA(Node):
 
     def get_video_source(self):
         root = tk.Tk()
-        root.geometry("300x200")
+        root.geometry("416x416")
         root.title("Select Video Source")
 
         def handle_button_click(selection):
@@ -94,20 +94,10 @@ class DetectorA(Node):
         img_rgb = cv2.cvtColor(img_bgr8, cv2.COLOR_BGR2RGB)
 
         # model
-        model = YOLO("yolo-Weights/yolov8n.pt")
+        model = YOLO("yolo-Weights/bestweigts.pt")
 
         # object classes
-        classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-                    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-                    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-                    "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-                    "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-                    "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-                    "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-                    "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-                    "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-                    "teddy bear", "hair drier", "toothbrush"
-                    ]
+        classNames = ["freshripe", "freshunripe", "overripe", "ripe", "rotten", "unripe"]
 
         # Initialize an empty list to store object information
         detected_objects = []      
