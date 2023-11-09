4975f579bb4519ecc0ef91ea09fe4ff9b3370790
@JuanR5 <juan_p.rivera@uao.edu.co>
2023-11-09 15:46:12 -0500

    All working, just some esthetic or tunning

--- smartg/src/smart_g/outputNode.py
+++ smartg/src/smart_g/outputNode.py
@@ -20,19 +20,18 @@ class OutputNode(Node):
 
         self.br = CvBridge()
         self.encoding = 'rgb8'
-        self.filtered_frame = None
         self.selected_topic = "tracked_obj"  # Default topic
-
-        self.subscription_dict = {}
         self.switching = False  # Flag to prevent rapid switching
-        self.switch_delay = 2  # Delay in seconds
+        self.switch_delay = 1 # Delay in seconds
+
+        self.video_subscription=None
 
         self.init_gui()
         self.start_video_subscription("tracked_obj")
         self.start_video_subscription("leaf_frames")
-        self.video_subscription = self.subscription_dict[self.selected_topic]
+        
         self.text_subscription = self.create_subscription(String, 'time_topic', self.text_callback, 10)
-        #self.start_label_subscription() #Label subscription 
+        self.label_subscription = None 
 
     def init_gui(self):
         self.root = tk.Tk()
@@ -41,25 +40,22 @@ class OutputNode(Node):
         self.video_frame = tk.Label(self.root)
         self.video_frame.pack()
 
-        self.text_frame = tk.Label(self.root, text="Result Message Here")
+        self.text_frame = tk.Label(self.root, text="Result Message Here", font=("Arial", 18, "italic"))
         self.text_frame.pack()
-        self.text_frame.config(font=("Arial", 15))
         
-        self.label2 = tk.Label(self.root, text="")
+        self.label2 = tk.Label(self.root, text="", font=("Arial", 18, "italic"))
         self.label2.pack()
-        self.label2.config(font=("Arial", 15))
 
         self.switch_button = tk.Button(self.root, text="Switch Video", command=self.switch_video)
         self.switch_button.pack()
-        # Set the size and color of the button
-        self.switch_button.config(height=10, width=10, background="#ff8c00", foreground="black")
-        # Set the font of the button text
-        self.switch_button.config(font=("Arial", 10, "bold"))
-    
-    def start_label_subscription(self):
-        if "label_topic" not in self.subscription_dict:
-            subscription = self.create_subscription(String, "label_topic", self.label_callback, 10)
-            self.subscription_dict["label_topic"] = subscription
+        self.switch_button.config(height=2, width=20, bg = 'green', foreground="black", font=("Arial", 18, "bold"))
+        
+        # Update and resize the GUI to fit its elements
+        """self.root.update_idletasks()
+        self.root.geometry('{}x{}'.format(
+            self.switch_button.winfo_width() + 20, 
+            self.switch_button.winfo_height() + self.label2.winfo_height() + 
+            self.text_frame.winfo_height() + self.video_frame.winfo_height() + 40) ) """
             
     def update_video_frame(self, frame):
         img_tk = self.cv2_to_image_tk(frame)
@@ -77,8 +73,9 @@ class OutputNode(Node):
             self.root.update()
 
     def start_video_subscription(self, topic):
-        subscription = self.create_subscription(Image, topic, self.video_callback, 10)
-        self.subscription_dict[topic] = subscription
+        if self.video_subscription:
+            self.destroy_subscription(self.video_subscription)
+        self.video_subscription = self.create_subscription(Image, topic, self.video_callback, 10)
 
     def video_callback(self, msg):
         img = self.br.imgmsg_to_cv2(msg, desired_encoding=self.encoding)
@@ -89,19 +86,13 @@ class OutputNode(Node):
         if not self.switching:
             self.switching = True
             new_topic = "leaf_frames" if self.selected_topic == "tracked_obj" else "tracked_obj"
-
-            # First subscribe to the new topic
-            self.start_label_subscription()
-            # Add a delay to prevent rapid switching
-            time.sleep(self.switch_delay)          
+            
+            # Update the selected topic and subscribe to the new topic
+            self.selected_topic = new_topic
             self.start_video_subscription(new_topic)
 
-            # Then unsubscribe from the current topic
-            self.destroy_subscription(self.video_subscription)
-            # Finally, update the selected topic variable
-            self.selected_topic = new_topic
-            self.video_subscription = self.subscription_dict[self.selected_topic]
             self.switching = False
+
             
     def cv2_to_image_tk(self, cv_image):
         bridge = CvBridge()
